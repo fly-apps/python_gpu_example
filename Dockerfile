a1c145722d958a5860db1f159233b69d29c9d8aa
Chris Nicoll <c@fly.io>
2023-11-21 11:07:30 -0500

    use venv

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM ubuntu:22.04
-RUN apt update -q && apt install -y python3 python3-pip git git-lfs nano && \
+RUN apt update -q && apt install -y python3 python3-pip python3-venv git git-lfs nano && \
     apt clean && rm -f /var/lib/apt/lists/*_*
 
-ENV PYTHON_USER="pythonuser"
+ARG NONROOT_USER
+RUN echo "User will be $NONROOT_USER"
+ENV PYTHON_USER=$NONROOT_USER
 
-SHELL ["/bin/bash", "-c"]
+# SHELL ["/bin/bash", "-c"]
 
 # Create unprivileged user with a home dir and using bash
 RUN useradd -ms /bin/bash $PYTHON_USER
-RUN mkdir /app && chown $PYTHON_USER:$PYTHON_USER /app
-WORKDIR /app
+# RUN mkdir /app && chown $PYTHON_USER:$PYTHON_USER /app
+# WORKDIR /app
 
 COPY --chmod=0755 ./entrypoint.sh ./entrypoint.sh
 COPY --chown=$PYTHON_USER:$PYTHON_USER --chmod=0755 ./post-initialization.sh ./post-initialization.sh
--- post-initialization.sh
+++ post-initialization.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-echo "Running post-initialization script as $USER."
+PROJECT_DIR="project"
+VENV_DIR="$PROJECT_DIR-venv"
 
-export PATH=$PATH:~/.local/bin
+echo "Running post-initialization script as $USER."
+echo "Entering $USER's home dir"
+# echo "Adding ~/.local/bin to PATH"
+# export PATH=$PATH:~/.local/bin
+cd ~
+echo "Creating venv dir for $PROJECT_DIR if it doesn't exist"
+mkdir -p $VENV_DIR 
+echo "Creating venv in $VENV_DIR if it doesn't exist"
+
+if [ ! -f "$VENV_DIR/pyvenv.cfg" ]; then
+    echo "No 'pyvenv.cfg' file found in $VENV_DIR; creating a venv"
+    python3 -m venv $VENV_DIR
+fi
+
+echo "Activating venv in $VENV_DIR"
+source $VENV_DIR/bin/activate
+
+echo "Creating dir $PROJECT_DIR if it doesn't exist"
+mkdir -p $PROJECT_DIR && cd $PROJECT_DIR
+
 
 if pip show jupyter &> /dev/null; then
     echo "Jupyter is installed with pip."
 else
-    echo "export PATH=$PATH:~/.local/bin" > ~/.bashrc 
+    # echo "Adding ~/.local/bin to PATH in .bashrc"
+    # echo "export PATH=$PATH:~/.local/bin" > ~/.bashrc 
     echo "Installing packages with pip"
-    pip install jupyter --user
-    # torch diffusers transformers accelerate --user
+    # Adapt the pip packages to match your project's needs. You can also install more after deployment. This Python venv lives on the persistent Fly Volume.
+    # For some reason numpy isn't getting installed as a dependency of torch, so install it explicitly
+    pip install jupyter # numpy torch 
+    # pip install diffusers transformers accelerate # HuggingFace libs for specific projects
 fi
 
-echo "About to cd into $USER's home dir"
-cd ~
-echo "Starting jupyter notebook!"
+echo "Starting Jupyter notebook server!"
 jupyter notebook --ip $FLY_PRIVATE_IP --no-browser 
-
+# If you don't want Jupyter, use `sleep inf` instead, and `fly ssh console` into the Machine to 
+# interact with it.
 # sleep inf
